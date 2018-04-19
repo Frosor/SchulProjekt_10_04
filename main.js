63a00132dfe56c13de81b5b80e8e2f4a51bc5ec8
Doome161 <38245052+Doome161@users.noreply.github.com>
2018-04-19 08:29:05 +0200

    Add files via upload
    Funktioniert noch besser. Benutzerfreundlichkeit gesteigert & Kommentare hinzugefügt

--- main.js
+++ main.js
@@ -3,12 +3,9 @@ $(function () { //Document Ready in Kurz
     var Kreis;
     var Schwierigkeit_;
     var Trys;
-    var spam = true;
-    var toptime = 0;
-    $("#NeuerVersuch").prop("disabled", true);
     var spielfeld_leicht = '<div style="top:50px;" class="ReaktionsKreis mx-auto Kreis" id="ReaktionsKreis0"></div>';
-    var spielfeld_normal = '<div class="wrapper"><div class="ReaktionsKreis2 Kreis" id="ReaktionsKreis1"></div> <div class="ReaktionsKreis2 Kreis" id="ReaktionsKreis2"></div>	<div class="ReaktionsKreis2 Kreis" id="ReaktionsKreis3"></div> <div class="ReaktionsKreis2 Kreis" id="ReaktionsKreis4"></div></div>';
-    var spielfeld_schwer = '<div class="wrapper"><div class="ReaktionsKreis3 Kreis" id="ReaktionsKreis5"></div> <div class="ReaktionsKreis3 Kreis" id="ReaktionsKreis6"></div>	<div class="ReaktionsKreis3 Kreis" id="ReaktionsKreis7"></div> <div class="ReaktionsKreis3 Kreis" id="ReaktionsKreis8"></div><div class="ReaktionsKreis3 Kreis" id="ReaktionsKreis9"></div> <div class="ReaktionsKreis3 Kreis" id="ReaktionsKreis10"></div>	<div class="ReaktionsKreis3 Kreis" id="ReaktionsKreis11"></div> <div class="ReaktionsKreis3 Kreis" id="ReaktionsKreis12"></div><div class="ReaktionsKreis3 Kreis" id="ReaktionsKreis13"></div></div>';
+    var spielfeld_normal = '<div id="wrapper"><div class="ReaktionsKreis2 Kreis" id="ReaktionsKreis1"></div> <div class="ReaktionsKreis2 Kreis" id="ReaktionsKreis2"></div>	<div class="ReaktionsKreis2 Kreis" id="ReaktionsKreis3"></div> <div class="ReaktionsKreis2 Kreis" id="ReaktionsKreis4"></div></div>';
+    var spielfeld_schwer = '<div> Schwer Test Omg lul</div>';
 
     //Kreise Entfernen die sich im Speilfeld befinden
     function KreiseEntfernen() {
@@ -37,11 +34,6 @@ $(function () { //Document Ready in Kurz
             Kreis = "#ReaktionsKreis" + randomKreis;
         }
 
-        if (Schwierigkeit_ == "Schwer") {
-            var randomKreis = Math.floor(Math.random() * (13 - 5 + 1)) + 5;
-            Kreis = "#ReaktionsKreis" + randomKreis;
-        }
-
         //Start Button & Neuer Versuch Button disabln bis man in den Kreis geklickt hat.
         $(this).off();
         $("#NeuerVersuch").prop("disabled", true);
@@ -60,39 +52,24 @@ $(function () { //Document Ready in Kurz
         Trys++;
 
         $(".Kreis").one("click", function () {
-            
             //Aktuelle Farbe des Kreises am Moment des Klicks in eine Varibale speichern
             var color = $(this).css('background-color');
             //Mehrfach klicken auch auf andere Kreise verhindern (Normal, Schwer)
             $(".Kreis").off();
             if (color == "rgb(255, 255, 0)") {
-                
-                //Zeit des aktuellen Versuches und ID zur akutellen Zeit
-                var id = "zeit_div_" + Trys;
-                // Standard Abweichung (25), durch div. Aufrufe 
-                var time = new Date().getTime() - ms - 25;
-                
-               //berechnet Best-Zeit, wenn nicht vorhanden wird Best-Zeit als aktuelle zeit gesetzt 
-               if(toptime > time || toptime == 0) {
-                    toptime = time;
-                    $("#tt").empty();
-                    $("#bottom").prepend("<div id='tt'>" + toptime + " ms.</div>"); 
-                    
-                }
                 //Zeit in MS in die Zeiten Box; jewaliges div mit einer ID versehen damit man ein einzeln löschen kann wenn es zu viele werden
-                $("#Zeiten").prepend("<div id=" + id + "> " + Trys + ". " + time + " ms. </div>");
-                //Best-Zeit header in die Zeiten Box, Best-Zeit in MS in inhalt box
-                $("#Zeiten").prepend("<h5 id='toptime'> Best-Zeit: </h5>");
-                
+                var id = "zeit_div_" + Trys;
+                $("#Zeiten").prepend("<div id=" + id + "> " + Trys + ". " + (new Date().getTime() - ms) + " ms. </div>");
+                clearTimeout(Timer);
             }
             if (color != "rgb(255, 255, 0)") {
                 var id = "zeit_div_" + Trys;
-                $("#Zeiten").prepend("<div id=" + id + "> " + Trys + ". <b> Falsch! </b > <br></div>");
+                $("#Zeiten").prepend("<div id=" + id + "> " + Trys + ". <b> Zu Früh! </b > <br></div>");
+                //setTimeOut Stoppen
+                clearTimeout(Timer);
             }
-            //setTimeOut Stoppen
             clearTimeout(Timer);
             ZeitRemove();
-            spam = true;
             //Neuer Verusuch Button wieder aktivieren
             $("#NeuerVersuch").prop("disabled", false);
         });
@@ -100,20 +77,16 @@ $(function () { //Document Ready in Kurz
 
     //SchwierigkeitsButtons, Farbe ändern wenn Aktiv / Nicht Aktiv
     function Schwierigkeiten_Button_On_Off(event, An1, An2) {
-        //Aktuellen Button, alle Handles entfernen // Beide anderen Buttons click handle wieder drauf setzen
         $("#" + event.data.schwierigkeit).off();
         $("#" + An1).on("click", { schwierigkeit: "" + An1 }, Spielfeld_Erstellen);
         $("#" + An2).on("click", { schwierigkeit: "" + An2 }, Spielfeld_Erstellen);
 
-        //Klassen der Buttons ändern, sodass der Angeklicke seine Farbe zum "btn-dark" ändert
         $("#" + event.data.schwierigkeit).addClass("Not_Active_Button").removeClass("Active_Button");
         $(".Not_Active_Button").removeClass("btn-outline-dark").addClass("btn-dark");
         $(".Active_Button").removeClass("btn-dark").addClass("btn-outline-dark");
         $("#" + An1).addClass("Active_Button").removeClass("Not_Active_Button");
         $("#" + An2).addClass("Active_Button").removeClass("Not_Active_Button");
 
-        //StartButton "zurück" setzen, Alle Handles entfernen, und das neue Aktuelle click handle wieder drauf setzen
-        $("#StartButton").off();
         $("#StartButton").on("click", ReaktionMessen);
     }
 
@@ -121,7 +94,6 @@ $(function () { //Document Ready in Kurz
     function Spielfeld_Erstellen(event) {
         if (event.data.schwierigkeit == "Leicht") {
             spielfeld_aktuell = spielfeld_leicht;
-            //Aktuelle Schwierigkeit des geklickten Buttons via event.data
             Schwierigkeit_ = event.data.schwierigkeit;
             Schwierigkeiten_Button_On_Off(event, "Normal", "Schwer");
         }
@@ -135,13 +107,10 @@ $(function () { //Document Ready in Kurz
             Schwierigkeit_ = event.data.schwierigkeit;
             Schwierigkeiten_Button_On_Off(event, "Normal", "Leicht");
         }
+
         KreiseEntfernen();
         $("#Spielfeld").append(spielfeld_aktuell);
-        //beim Wechsel von Schwierigkeit Zeiten, Versuche und Best-Zeit entfernen
         $("#Zeiten").empty();
-        $("#tt").empty();
-        $("#NeuerVersuch").prop("disabled", true);
-        toptime = 0;
         Trys = 0;
     }
 
@@ -155,22 +124,15 @@ $(function () { //Document Ready in Kurz
 
     //Start Button wieder Aktivierern + Farbe des Kreises wieder auf ursprung
     $("#NeuerVersuch").click(function () {
-        if(spam == true) {
         $("#StartButton").on("click", ReaktionMessen);
         $(Kreis).css("background-color", "rgb(85, 85, 85)");
-        spam = false;
-        }
     });
-    
-    //$("#StartButton").click, ReaktionMessen);
 
     //Seite Neu Laden
     $("#ResetButton").click(function () {
         location.reload();
     });
 });
-
-
 
 
 
